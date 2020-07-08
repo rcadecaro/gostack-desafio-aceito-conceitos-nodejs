1c4cc13c84bef7934a999f950544b92629945970
Renato Cadecaro <rcadecaro@gmail.com>
2020-07-08 19:53:06 -0300

    chore: delete /repositories

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
   const repositoryIndex = repositories.findIndex(
-    (repositorie) => id === repositorie.id
+    (repository) => id === repository.id
   );
 
   if (repositoryIndex < 0)
@@ -47,7 +47,18 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => id === repository.id
+  );
+
+  if (repositoryIndex < 0)
+    return response.status(400).json({ error: "Não foi possível localizar o repositório!" });
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
