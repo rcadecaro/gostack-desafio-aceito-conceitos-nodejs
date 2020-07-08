f376ba4ed10ef53514b381fb5ff4bd3472e51b1e
Renato Cadecaro <rcadecaro@gmail.com>
2020-07-08 19:40:05 -0300

    chore: PUT /repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -26,7 +26,24 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const params = { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repositorie) => id === repositorie.id
+  );
+
+  if (repositoryIndex < 0)
+    return response.status(400).json({ error: "Não foi possível localizar o repositório!" });
+
+  repository = {
+    ...repositories[repositoryIndex],
+    ...params,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
